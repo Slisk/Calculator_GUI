076ee673063c768c412a591387d19a9fbcff87c8
Johan Moback <Johan Moback>
2016-11-02 11:20:08 +0100

    I have now added actionListener to all the numbers within the calculator. Also added action to clear btn.

--- src/Calculator_GUI.java
+++ src/Calculator_GUI.java
@@ -11,6 +11,9 @@ public class Calculator_GUI {
 
 	private JFrame frame;
 	private JTextField textField;
+	
+	double firstnum;
+	double secondnum;
 
 	/**
 	 * Launch the application.
@@ -45,6 +48,7 @@ public class Calculator_GUI {
 		frame.getContentPane().setLayout(null);
 		
 		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField.setBounds(0, 0, 534, 52);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
@@ -52,6 +56,9 @@ public class Calculator_GUI {
 		JButton btn7 = new JButton("7");
 		btn7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				String EnterNumber = textField.getText() + btn7.getText(); 
+				textField.setText(EnterNumber); 
 			}
 		});
 		btn7.setBounds(10, 75, 49, 40);
@@ -60,44 +67,109 @@ public class Calculator_GUI {
 		JButton btn8 = new JButton("8");
 		btn8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn8.getText(); 
+				textField.setText(EnterNumber);  
 			}
 		});
 		btn8.setBounds(73, 75, 49, 40);
 		frame.getContentPane().add(btn8);
 		
 		JButton btn9 = new JButton("9");
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn9.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn9.setBounds(134, 75, 49, 40);
 		frame.getContentPane().add(btn9);
 		
 		JButton btn4 = new JButton("4");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn4.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn4.setBounds(10, 128, 49, 40);
 		frame.getContentPane().add(btn4);
 		
 		JButton btn5 = new JButton("5");
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn5.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn5.setBounds(73, 128, 49, 40);
 		frame.getContentPane().add(btn5);
 		
 		JButton btn6 = new JButton("6");
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn6.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn6.setBounds(134, 128, 49, 40);
 		frame.getContentPane().add(btn6);
 		
 		JButton btn1 = new JButton("1");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn1.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn1.setBounds(10, 181, 49, 40);
 		frame.getContentPane().add(btn1);
 		
 		JButton btn2 = new JButton("2");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn2.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn2.setBounds(72, 181, 49, 40);
 		frame.getContentPane().add(btn2);
 		
 		JButton btn3 = new JButton("3");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn3.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn3.setBounds(134, 181, 49, 40);
 		frame.getContentPane().add(btn3);
 		
 		JButton btn0 = new JButton("0");
+		btn0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String EnterNumber = textField.getText() + btn0.getText(); 
+				textField.setText(EnterNumber); 
+			}
+		});
 		btn0.setBounds(73, 234, 49, 40);
 		frame.getContentPane().add(btn0);
 		
 		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				textField.setText(null);  
+			}
+		});
 		btnClear.setBounds(245, 75, 97, 40);
 		frame.getContentPane().add(btnClear);
 		
@@ -138,6 +210,12 @@ public class Calculator_GUI {
 		frame.getContentPane().add(btnAbs);
 		
 		JButton btnEquals = new JButton("=");
+		btnEquals.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
 		btnEquals.setBounds(409, 269, 55, 52);
 		frame.getContentPane().add(btnEquals);
 		
